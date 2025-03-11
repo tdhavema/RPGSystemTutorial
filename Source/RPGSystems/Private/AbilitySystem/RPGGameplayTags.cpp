9f1952590fbfd437e04f83ad0cf185bcef06452e
tdhavema <tdhavema@gmail.com>
2025-03-11 08:58:34 -0500

    Implement Crouch Pt1

--- Source/RPGSystems/Private/AbilitySystem/RPGGameplayTags.cpp
+++ Source/RPGSystems/Private/AbilitySystem/RPGGameplayTags.cpp
@@ -7,3 +7,8 @@ namespace RPGGameplayTags::Combat
 {
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Data_Damage, "Combat.Data.Damage", "Set By Caller Data Tag For Combat");
 }
+
+namespace RPGGameplayTags::Player::State
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Crouch, "Player.State.Crouch", "Player Is Actively Crouching");	
+}
